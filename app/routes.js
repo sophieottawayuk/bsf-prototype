f5a1bbcf010f47758d09ddae9736bb6d167f2824
sophieottawayuk <sophie@ottawaymedia.co.uk>
2020-05-25 16:32:23 +0100

    Adjustments to routing

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,13 @@ var responsibleEntity;
 var yourDetailsSectionComplete;
 var responsibleEntitySectionComplete;
 
-// Add your routes here - above the module.exports line
+/* Set all sections to incomplete on load*/
+router.get('/', function (req, res) {
+  yourDetailsSectionComplete = false;
+  responsibleEntitySectionComplete = false;
+  res.render('index');
+})
+
 router.post('/start-journey', function (req,res) {
 res.redirect('task-list');
 });
@@ -20,11 +26,10 @@ router.post('/responsible-entity-submit', function (req,res) {
 res.redirect('task-list');
 });
 
-router.post('/responsible-entity-handler', function (req, res) {
 
+router.post('/responsible-entity-handler', function (req, res) {
   responsibleEntity = req.session.data['applicant']['responsible-entity'];
   console.log(responsibleEntity);
-
   if (responsibleEntity === 'no') {
     responsibleEntity = true;
     res.redirect('/responsible-entity/name')
@@ -35,14 +40,33 @@ router.post('/responsible-entity-handler', function (req, res) {
 })
 
 router.get('/responsible-entity/check-answers', function (req, res) {
-  console.log("It loaded");
-  console.log(responsibleEntity);
   res.render('responsible-entity/check-answers', { responsibleEntity })
 })
 
 router.get('/task-list', function (req, res) {
   res.render('task-list', { responsibleEntity, responsibleEntitySectionComplete, yourDetailsSectionComplete })
 })
+
+
+/*Routing to allow return to start of section or check answers depending on whether section completed*/
+router.get('/your-details/about-you-routing', function (req, res) {
+if (yourDetailsSectionComplete == true) {
+  res.render('your-details/check-answers');
+}
+else {
+  res.render('your-details/name');
+}
+})
+
+router.get('/responsible-entity/responsible-entity-routing', function (req, res) {
+if (responsibleEntitySectionComplete == true) {
+  res.render('responsible-entity/check-answers');
+}
+else {
+  res.render('responsible-entity/index');
+}
+})
+
 
 
 module.exports = router
